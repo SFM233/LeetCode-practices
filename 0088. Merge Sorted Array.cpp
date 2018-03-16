f7feea786c54ad0b2bbb50ea912d0b64a1895bc7
SFM233 <ender_sfm@163.com>
2018-03-16 11:12:04 +0800

    Two easy problems

---
+++ 0088. Merge Sorted Array.cpp
@@ -0,0 +1,16 @@
+void merge(int* nums1, int m, int* nums2, int n) {
+    int a = m-1;
+    int b = n-1;
+    int all = m + n - 1;
+    while(a>=0&&b>=0){
+        if(nums1[a]>nums2[b]){
+            nums1[all--] = nums1[a--];
+        }
+        else{
+            nums1[all--] = nums2[b--];
+        }
+    }
+    while(b>=0){
+        nums1[all--] = nums2[b--];
+    }
+}
